dbfb1ff3525a4378bd62cc2d00844398e5eff2f2
Thiago Faquinello <thiagojanatre@gmail.com>
2026-09-17 22:03:21 -0300

    feat: Adiciona exercícios do Dia 33

---
+++ 01-Estudos/Dia 33/Exercício01.py
@@ -0,0 +1,6 @@
+numeros = [3, 7, 10, 14, 18, 21]
+
+lista_pares = [numero*2 for numero in numeros if numero%2==0]
+
+print(lista_pares)
+
---
+++ 01-Estudos/Dia 33/Exercício02.py
@@ -0,0 +1,10 @@
+produtos = [
+    {"nome": "Motor", "preco": 1500, "estoque": 10},
+    {"nome": "Sensor", "preco": 250, "estoque": 3},
+    {"nome": "Arduino", "preco": 180, "estoque": 8},
+    {"nome": "CLP", "preco": 3200, "estoque": 2}
+]
+
+produtos_acima_500 = [produto["nome"] for produto in produtos if produto["preco"]>500]
+
+print(produtos_acima_500)
